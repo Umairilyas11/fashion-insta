1a9ef01c5035949dc09fd9b9116ae9b6c67563b9
Umairilyas11 <umairgdw@gmail.com>
2023-10-09 00:22:47 +0530

    Footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,36 +6,75 @@ import Grid from "@mui/material/Grid";
 import { Typography } from "@mui/material";
 import Form from "react-bootstrap/Form";
 import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
-import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import { makeStyles } from "@mui/styles";
+import { Link } from "react-router-dom";
 
 const Item = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(1),
   textAlign: "center",
+  color: "white",
+  backgroundColor: "transparent",
+  boxShadow: "none",
+  marginTop: "1rem",
+  marginBottom: "1rem",
+}));
 
-  //   backgroundColor: "transparent",
-  //   boxShadow: "none",
+const useStyles = makeStyles((theme) => ({
+  Email: {
+    backgroundColor: "transparent",
+    borderRadius: "35px",
+    maxWidth: "25rem",
+    border: "none",
+  },
+
+  LinkSocial: {
+    textDecoration: "none",
+    color: "white",
+    border: "1px solid white",
+    padding: "4px 10px 4px 10px",
+    borderRadius: "35px",
+  },
+  Link: {
+    textDecoration: "none",
+    color: "white",
+    textAlign: "left",
+  },
 }));
 
 function Footer() {
+  const classes = useStyles();
   return (
-    <Box sx={{ flexGrow: 1, backgroundColor: "#320001" }}>
+    <Box
+      sx={{
+        flexGrow: 1,
+        backgroundColor: "#320001",
+      }}
+    >
       <Grid container spacing={3}>
         <Grid item xs>
           <Item></Item>
         </Grid>
         <Grid item xs={11}>
           <Item>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-              }}
-            >
+            <div>
               <Box sx={{ flexGrow: 1 }}>
-                <Grid container spacing={3}>
+                <Grid
+                  container
+                  spacing={3}
+                  sx={{ display: "flex", justifyContent: "center" }}
+                >
                   <Grid item xs={5}>
                     <Item>
-                      <Typography>
+                      <Typography
+                        sx={{
+                          fontSize: "2rem",
+                          textAlign: "left",
+                          fontFamily: "Noto Looped Thai",
+                          maxWidth: "27rem",
+                          marginBottom: "1rem",
+                        }}
+                      >
                         Having something to talk about our professionals?
                       </Typography>
                       <div
@@ -49,53 +88,242 @@ function Footer() {
                           <Form.Group
                             className="mb-3"
                             controlId="exampleForm.ControlInput1"
-                            style={{ display: "flex", alignItems: "center" }}
+                            style={{
+                              display: "flex",
+                              alignItems: "center",
+                              border: "1px solid white",
+                              borderRadius: "35px",
+                              maxWidth: "25rem",
+                              "&::selection": {
+                                backgroundColor: "transparent",
+                              },
+                            }}
                           >
                             <Form.Control
                               type="email"
                               placeholder="Enter email here"
+                              className={classes.Email}
                             />
-                            <Button
-                              variant="contained"
-                              startIcon={<ArrowOutwardIcon />}
-                            ></Button>
+
+                            <IconButton
+                              sx={{
+                                backgroundColor: "#F15836",
+                              }}
+                            >
+                              <ArrowOutwardIcon />
+                            </IconButton>
                           </Form.Group>
                         </Form>
                         <div
                           style={{
                             display: "flex",
                             alignItems: "center",
-                            gap: "5px",
+                            gap: "7px",
+                            paddingTop: "5rem",
                           }}
                         >
-                          <Typography>
-                            WhatsApp
-                            <ArrowOutwardIcon />
-                          </Typography>
-                          <Typography>
-                            WhatsApp
-                            <ArrowOutwardIcon />
-                          </Typography>
-                          <Typography>
-                            WhatsApp
-                            <ArrowOutwardIcon />
-                          </Typography>
-                          <Typography>
-                            WhatsApp
-                            <ArrowOutwardIcon />
-                          </Typography>
+                          <Link className={classes.LinkSocial}>
+                            <Typography
+                              sx={{
+                                fontFamily: "open sans",
+                                fontWeight: "lighter",
+                              }}
+                            >
+                              WhatsApp
+                              <ArrowOutwardIcon />
+                            </Typography>
+                          </Link>
+                          <Link className={classes.LinkSocial}>
+                            <Typography
+                              sx={{
+                                fontFamily: "open sans",
+                                fontWeight: "lighter",
+                              }}
+                            >
+                              Email
+                              <ArrowOutwardIcon />
+                            </Typography>
+                          </Link>
+                          <Link className={classes.LinkSocial}>
+                            <Typography
+                              sx={{
+                                fontFamily: "open sans",
+                                fontWeight: "lighter",
+                              }}
+                            >
+                              Twitter
+                              <ArrowOutwardIcon />
+                            </Typography>
+                          </Link>
+                          <Link className={classes.LinkSocial}>
+                            <Typography
+                              sx={{
+                                fontFamily: "open sans",
+                                fontWeight: "lighter",
+                              }}
+                            >
+                              Instagram
+                              <ArrowOutwardIcon />
+                            </Typography>
+                          </Link>
                         </div>
                       </div>
                     </Item>
                   </Grid>
                   <Grid item xs={2}>
-                    <Item>xs=2</Item>
+                    <Item>
+                      <Link className={classes.Link}>
+                        <Typography
+                          sx={{
+                            marginBottom: "3rem",
+                            fontFamily: "open sans",
+                            fontWeight: "bold",
+                          }}
+                        >
+                          Menu
+                        </Typography>
+                      </Link>
+                      <Link className={classes.Link}>
+                        <Typography
+                          sx={{
+                            marginBottom: "5px",
+                            fontFamily: "open sans",
+                            fontWeight: "lighter",
+                          }}
+                        >
+                          About Us
+                        </Typography>
+                      </Link>{" "}
+                      <Link className={classes.Link}>
+                        <Typography
+                          sx={{
+                            marginBottom: "5px",
+                            fontFamily: "open sans",
+                            fontWeight: "lighter",
+                          }}
+                        >
+                          Newsroom
+                        </Typography>
+                      </Link>{" "}
+                      <Link className={classes.Link}>
+                        <Typography
+                          sx={{
+                            marginBottom: "5px",
+                            fontFamily: "open sans",
+                            fontWeight: "lighter",
+                          }}
+                        >
+                          Careers
+                        </Typography>
+                      </Link>{" "}
+                      <Link className={classes.Link}>
+                        <Typography
+                          sx={{
+                            marginBottom: "5px",
+                            fontFamily: "open sans",
+                            fontWeight: "lighter",
+                          }}
+                        >
+                          Partnerships
+                        </Typography>
+                      </Link>
+                    </Item>
                   </Grid>
                   <Grid item xs={2}>
-                    <Item>xs=2</Item>
+                    <Item>
+                      <Link className={classes.Link}>
+                        <Typography
+                          sx={{
+                            marginBottom: "3rem",
+                            fontFamily: "open sans",
+                            fontWeight: "bold",
+                          }}
+                        >
+                          Get Help
+                        </Typography>
+                      </Link>{" "}
+                      <Link className={classes.Link}>
+                        <Typography
+                          sx={{
+                            marginBottom: "5px",
+                            fontFamily: "open sans",
+                            fontWeight: "lighter",
+                          }}
+                        >
+                          Support Carrer
+                        </Typography>
+                      </Link>{" "}
+                      <Link className={classes.Link}>
+                        <Typography
+                          sx={{
+                            marginBottom: "5px",
+                            fontFamily: "open sans",
+                            fontWeight: "lighter",
+                          }}
+                        >
+                          24h Service
+                        </Typography>
+                      </Link>{" "}
+                      <Link className={classes.Link}>
+                        <Typography
+                          sx={{
+                            marginBottom: "5px",
+                            fontFamily: "open sans",
+                            fontWeight: "lighter",
+                          }}
+                        >
+                          Quick Chat
+                        </Typography>
+                      </Link>
+                    </Item>
                   </Grid>
                   <Grid item xs={2}>
-                    <Item>xs=2</Item>
+                    <Item>
+                      <Link className={classes.Link}>
+                        <Typography
+                          sx={{
+                            marginBottom: "3rem",
+                            fontFamily: "open sans",
+                            fontWeight: "bold",
+                          }}
+                        >
+                          Support
+                        </Typography>
+                      </Link>{" "}
+                      <Link className={classes.Link}>
+                        <Typography
+                          sx={{
+                            marginBottom: "5px",
+                            fontFamily: "open sans",
+                            fontWeight: "lighter",
+                          }}
+                        >
+                          FAQ
+                        </Typography>
+                      </Link>{" "}
+                      <Link className={classes.Link}>
+                        <Typography
+                          sx={{
+                            marginBottom: "5px",
+                            fontFamily: "open sans",
+                            fontWeight: "lighter",
+                          }}
+                        >
+                          Policy
+                        </Typography>
+                      </Link>{" "}
+                      <Link className={classes.Link}>
+                        <Typography
+                          sx={{
+                            marginBottom: "5px",
+                            fontFamily: "open sans",
+                            fontWeight: "lighter",
+                          }}
+                        >
+                          Business
+                        </Typography>
+                      </Link>
+                    </Item>
                   </Grid>
                 </Grid>
               </Box>
